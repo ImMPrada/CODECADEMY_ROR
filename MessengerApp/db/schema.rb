5933395a7a681d7cdb9e87e24121880cca66023e
= <mprada.ing@gmail.com>
2021-04-01 22:53:00 -0500

    messenger app, finishing

--- MessengerApp/db/schema.rb
+++ MessengerApp/db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_29_010103) do
+ActiveRecord::Schema.define(version: 2021_04_02_024051) do
 
   create_table "messages", force: :cascade do |t|
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
