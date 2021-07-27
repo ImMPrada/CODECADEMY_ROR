98040a2895ed1f37526c806e732f963193a1ddf0
Miguel Prada <mprada.ing@gmail.com>
2021-07-27 12:06:13 -0500

    the-blog. devise and scaffolding Post

--- the-blog/config/routes.rb
+++ the-blog/config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  devise_for :authors
+  root to: "home#index"
+  
+  get 'home/index'
+  scope module: 'authors' do
+    resources :posts
+  end
+
 end
